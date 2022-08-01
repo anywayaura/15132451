167c5ef7954ed5e35b6cf462bbbc1ec3e538e9ce
hyperboria <anywayaura@gmail.com>
2022-08-01 22:59:59 +0700

    constants moved flatten code except is more presice dry code processing separated from reporting errors return variables precision

--- main.py
+++ main.py
@@ -4,59 +4,56 @@ from urllib.parse import urlparse
 
 import requests
 
-logging.basicConfig(level=logging.INFO)
-
+TOKEN = os.getenv('BITLY_TOKEN')
+logging.basicConfig(level=logging.DEBUG)
 
 def shorten_link(url, token):
-    headers = {'Authorization': f'Bearer {token}',
-               'Content-Type': 'application/json'}
-    data = {"long_url": url}
-    response = requests.post('https://api-ssl.bitly.com/v4/bitlinks', headers=headers, json=data)
+    headers = {'Authorization': f'Bearer {token}'}
+    long_url = {"long_url": url}
+    response = requests.post('https://api-ssl.bitly.com/v4/bitlinks', headers=headers, json=long_url)
     if response.ok:
         return response.json().get('link')
     else:
-        logging.error('shortening link failed')
+        logging.error('shortening link failed. ')
+        return
 
 
 def get_clicks(url, token):
-    headers = {'Authorization': f'Bearer {token}',
-               'Content-Type': 'application/json'}
-    params = {'unit': 'day',
-              'units': '-1'}
+    headers = {'Authorization': f'Bearer {token}'}
     parsed_url = urlparse(url)
     bitlink = f'{parsed_url.netloc}{parsed_url.path}'
     response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary',
-                            headers=headers,
-                            params=params)
+                            headers=headers)
     if response.ok:
-        return f'Clicks: {response.json().get("total_clicks")}'
+        return response.json().get("total_clicks")
     else:
         logging.error('getting clicks on bitlink failed')
+        return
 
 
 def is_bitlink(url, token):
     parsed_url = urlparse(url)
     bitlink = f'{parsed_url.netloc}{parsed_url.path}'
-    headers = {'Authorization': f'Bearer {token}',
-               'Content-Type': 'application/json'}
+    headers = {'Authorization': f'Bearer {token}'}
     response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}', headers=headers)
-    if response.ok:
-        return True
-    else:
-        return False
+    return response.ok
 
 
-def main():
-    TOKEN = os.getenv('BITLY_TOKEN')
+def main(token):
     url = input('Please Enter the link: ')
-    try:
-        if is_bitlink(url, TOKEN):
-            print(get_clicks(url, TOKEN))
-        else:
-            print(shorten_link(url, TOKEN))
-    except Exception as ex:
-        logging.error(f'error {ex}')
+    if urlparse(url).netloc:
+        try:
+            if is_bitlink(url, token):
+                print(f'Clicks: {get_clicks(url, token)}')
+            else:
+                print(f'Bitlink: {shorten_link(url, token)}')
+        except ConnectionError as ex:
+            logging.error(f'Internet Problems: {ex}')
+
+    else:
+        logging.error(f'Bad Link')
+
 
 
 if __name__ == '__main__':
-    main()
+    main(TOKEN)
