de038b6f9d4349d470e5710db580b888f433377e
hyperboria <anywayaura@gmail.com>
2022-08-02 14:16:43 +0700

    better exceptions no more constants rip

--- main.py
+++ main.py
@@ -4,18 +4,14 @@ from urllib.parse import urlparse
 
 import requests
 
-TOKEN = os.getenv('BITLY_TOKEN')
 logging.basicConfig(level=logging.INFO)
 
 def shorten_link(url, token):
     headers = {'Authorization': f'Bearer {token}'}
     long_url = {"long_url": url}
     response = requests.post('https://api-ssl.bitly.com/v4/bitlinks', headers=headers, json=long_url)
-    if response.ok:
-        return response.json().get('link')
-    else:
-        logging.error('shortening link failed. ')
-        return
+    response.raise_for_status()
+    return response.json().get('link')
 
 
 def get_clicks(url, token):
@@ -24,11 +20,9 @@ def get_clicks(url, token):
     bitlink = f'{parsed_url.netloc}{parsed_url.path}'
     response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary',
                             headers=headers)
-    if response.ok:
-        return response.json().get("total_clicks")
-    else:
-        logging.error('getting clicks on bitlink failed')
-        return
+    response.raise_for_status()
+    return response.json().get("total_clicks")
+
 
 
 def is_bitlink(url, token):
@@ -39,7 +33,8 @@ def is_bitlink(url, token):
     return response.ok
 
 
-def main(token):
+def main():
+    token = os.getenv('BITLY_TOKEN')
     url = input('Please Enter the link: ')
     if urlparse(url).netloc:
         try:
@@ -49,11 +44,11 @@ def main(token):
                 print(f'Bitlink: {shorten_link(url, token)}')
         except ConnectionError as ex:
             logging.error(f'Internet Problems: {ex}')
-
+        except requests.exceptions.HTTPError:
+            logging.error(f'What you are looking for cannot be found.')
     else:
         logging.error(f'Bad Link')
 
 
-
 if __name__ == '__main__':
-    main(TOKEN)
+    main()
